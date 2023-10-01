9e9c3586971bb26a96db59b9af110d05d1b29d5b
ebi <ebrahimmoosavi2010@gmail.com>
2023-10-01 12:48:24 +0330

    Update config file

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import torch.cuda
 
 lr = 0.0005
-wd = 1e-4
+wd = 0.0001
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-num_epochs = 10
-train_batch_size = 256
+num_epochs = 30
+train_batch_size = 512
 valid_batch_size = 512
