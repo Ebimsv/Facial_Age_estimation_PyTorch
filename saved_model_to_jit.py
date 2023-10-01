c6921570818462a84ea13a6c6f0a7a04fc7c086c
ebi <ebrahimmoosavi2010@gmail.com>
2023-10-01 12:53:14 +0330

    convert .pt to .jit

--- saved_model_to_jit.py
+++ saved_model_to_jit.py
@@ -1,11 +1,9 @@
-import timm
 import torch
-from train import ImageTaggerModel
+from model import AgeEstimationModel
 
-
-model = ImageTaggerModel(pretrained=True, num_classes=2)
-model.load_state_dict(torch.load("epoch:28-acc_valid:0.9588607549667358-acc_train:0.9734758734703064.pt"))
+model = AgeEstimationModel(input_dim=3, output_nodes=1, model_name='resnet', pretrain_weights='IMAGENET1K_V2')
+model.load_state_dict(torch.load("checkpoints/epoch:28-loss_valid:5.95.pt"))
 model.to('cuda')
 model.eval()
 model_jit = torch.jit.script(model)
-torch.jit.save(model_jit, 'model_jit.pt')
+torch.jit.save(model_jit, 'model-jit.pt')
