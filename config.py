dfd5bae91847e0eb5de0b4c57707fb1c065a0d70
Ebimsv <ebimsv0501@gmail.com>
2024-04-02 10:31:36 +0000

    decrease batch size

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ lr = 0.0005
 wd = 0.0001
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 num_epochs = 30
-train_batch_size = 512
-valid_batch_size = 512
+train_batch_size = 64
+valid_batch_size = 128
