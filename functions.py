0a429c18ddd2dbdfd0dabf5b35c873b8d3942f50
Ebimsv <ebimsv0501@gmail.com>
2024-04-06 10:10:37 +0000

    add metric alongside loss_fn

--- functions.py
+++ functions.py
@@ -4,41 +4,47 @@ from tqdm import tqdm
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-
-def train_one_epoch(model, train_loader, loss_fn, optimizer, metric='mae', epoch=1):
+def train_one_epoch(model, train_loader, loss_fn, optimizer, metric, epoch=1):
     model.train()
     loss_train = AverageMeter()
+    
+    metric.reset()
+
     with tqdm(train_loader, unit="batch") as tepoch:
         for inputs, targets, _, _ in tepoch:
             if epoch is not None:
                 tepoch.set_description(f"Epoch {epoch}")
-            inputs = inputs.to(device)
-            targets = targets.to(device)
-
+            inputs, targets = inputs.to(device), targets.to(device)
+            
             outputs = model(inputs)
 
-            loss = loss_fn(outputs, targets.unsqueeze(1).float().to(device))
+            loss = loss_fn(outputs, targets)
 
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
 
-            loss_train.update(loss.item())
-            tepoch.set_postfix(loss=loss_train.avg)
-        return model, loss_train.avg
+            loss_train.update(loss.item(), n=len(targets))
+            metric.update(outputs, targets)
+
+            tepoch.set_postfix(loss=loss_train.avg, metric=metric.compute().item())
+
+        return model, loss_train.avg, metric.compute().item()
 
 
-def validation(model, valid_loader, loss_fn):
+def validation(model, valid_loader, loss_fn, metric):
     model.eval()
+    metric.reset()
+
     with torch.no_grad():
         loss_valid = AverageMeter()
         for i, (inputs, targets, _, _) in enumerate(valid_loader):
-            inputs = inputs.to(device)
-            targets = targets.to(device)
+            inputs, targets = inputs.to(device), targets.to(device)
 
             outputs = model(inputs)
 
             loss = loss_fn(outputs, targets.unsqueeze(1).float().to(device))
-            loss_valid.update(loss.item())
+            loss_valid.update(loss.item(), n=len(targets))
+            metric(outputs, targets)
 
-    return loss_valid.avg
+    return loss_valid.avg, metric.compute().item()
