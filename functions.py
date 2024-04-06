7f5b116ffb0a44ee81eb3f1672209c3e8d59cc8e
Ebimsv <ebimsv0501@gmail.com>
2024-04-06 15:55:01 +0000

    reformat tqdm

--- functions.py
+++ functions.py
@@ -1,8 +1,7 @@
 import torch
 from utils import AverageMeter
 from tqdm import tqdm
-
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+import config
 
 def train_one_epoch(model, train_loader, loss_fn, optimizer, metric, epoch=1):
     model.train()
@@ -10,11 +9,11 @@ def train_one_epoch(model, train_loader, loss_fn, optimizer, metric, epoch=1):
     
     metric.reset()
 
-    with tqdm(train_loader, unit="batch") as tepoch:
-        for inputs, targets, _, _ in tepoch:
-            if epoch is not None:
-                tepoch.set_description(f"Epoch {epoch}")
-            inputs, targets = inputs.to(device), targets.to(device)
+    with tqdm(train_loader, unit="batch", desc=f'Epoch: {epoch+1}/{config['epochs']} ',
+              bar_format='{desc:<16}{percentage:3.0f}%|{bar:70}{r_bar}') as tqdm_dataloader:
+        
+        for inputs, targets, _, _ in tqdm_dataloader:
+            inputs, targets = inputs.to(config['device']), targets.to(config['device'])
             
             outputs = model(inputs)
 
@@ -23,28 +22,39 @@ def train_one_epoch(model, train_loader, loss_fn, optimizer, metric, epoch=1):
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
-
+            
+            # By setting n to len(targets), we ensure, that the loss is accurately calculated and updated, regardless of any changes in batch size.
             loss_train.update(loss.item(), n=len(targets))
             metric.update(outputs, targets)
 
-            tepoch.set_postfix(loss=loss_train.avg, metric=metric.compute().item())
-
+            tqdm_dataloader.set_postfix(loss=loss_train.avg, metric=metric.compute().item())
+        
+        del outputs
+        torch.cuda.empty_cache()
         return model, loss_train.avg, metric.compute().item()
 
 
 def validation(model, valid_loader, loss_fn, metric):
     model.eval()
+    loss_valid = AverageMeter()
+
     metric.reset()
 
-    with torch.no_grad():
-        loss_valid = AverageMeter()
-        for i, (inputs, targets, _, _) in enumerate(valid_loader):
-            inputs, targets = inputs.to(device), targets.to(device)
+    with tqdm(valid_loader, unit="batch", desc=f'Evaluating... ', 
+              bar_format='{desc:<16}{percentage:3.0f}%|{bar:70}{r_bar}') as tqdm_dataloader:
 
-            outputs = model(inputs)
+        with torch.no_grad():
+            for i, (inputs, targets, _, _) in enumerate(tqdm_dataloader):
+                inputs, targets = inputs.to(config['device']), targets.to(config['device'])
 
-            loss = loss_fn(outputs, targets.unsqueeze(1).float().to(device))
-            loss_valid.update(loss.item(), n=len(targets))
-            metric(outputs, targets)
+                outputs = model(inputs)
 
+                loss = loss_fn(outputs, targets)
+
+                # `n=len(targets)` for Dynamic Batch Size Consideration
+                loss_valid.update(loss.item(), n=len(targets))
+                metric.update(outputs, targets)
+                
+    del outputs
+    torch.cuda.empty_cache()
     return loss_valid.avg, metric.compute().item()
