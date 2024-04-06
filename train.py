468c734d409f002afd30ca50dac542ce4a6c03b0
Ebimsv <ebimsv0501@gmail.com>
2024-04-06 16:00:57 +0000

    debugging

--- train.py
+++ train.py
@@ -1,14 +1,17 @@
 from torch import nn, optim
+from torch.utils.tensorboard import SummaryWriter
+import torchmetrics as tm
+
+import os
+import logging
+import matplotlib.pyplot as plt
 
 import config
 from model import AgeEstimationModel
-import matplotlib.pyplot as plt
-from torch.utils.tensorboard import SummaryWriter
-
-import os
 from functions import train_one_epoch, validation
 from custom_dataset_dataloader import train_loader, valid_loader
 from config import *
+
 
 # create a folder to save checkpoints
 path = 'checkpoints'
@@ -19,8 +22,9 @@ except OSError as error:
 
 # Step 5: Train model for longer epochs using the best model from step 4 in hyperparameters_tuning.
 # Define model, define optimizer and Set learning rate and weight decay.
-model = AgeEstimationModel(input_dim=3, output_nodes=1, model_name='resnet', pretrain_weights='IMAGENET1K_V2').to(device)
+model = AgeEstimationModel(input_dim=3, output_nodes=1, model_name='resnet', pretrain_weights='IMAGENET1K_V2').to(config.device)
 loss_fn = nn.L1Loss()
+metric = tm.MeanAbsoluteError().to(device)
 optimizer = optim.SGD(model.parameters(), lr=config.lr, momentum=0.9, weight_decay=config.wd)
 
 # Write code to train the model for num_epochs epochs.
@@ -30,26 +34,31 @@ before_model_path = None
 loss_train_hist = []
 loss_valid_hist = []
 
-acc_train_hist = []
-acc_valid_hist = []
+metric_train_hist = []
+metric_valid_hist = []
 
 writer = SummaryWriter()
 
 
 for epoch in range(config.num_epochs):
     # Train
-    model, loss_train = train_one_epoch(model,
-                                        train_loader,
-                                        loss_fn,
-                                        optimizer,
-                                        epoch)
+    model, loss_train, metric_train = train_one_epoch(model,
+                                           train_loader,
+                                           loss_fn,
+                                           optimizer,
+                                           metric,
+                                           epoch)
     # Validation
-    loss_valid = validation(model,
-                            valid_loader,
-                            loss_fn)
+    loss_valid, metric_valid = validation(model,
+                                          valid_loader,
+                                          loss_fn, 
+                                          metric)
 
     loss_train_hist.append(loss_train)
     loss_valid_hist.append(loss_valid)
+
+    metric_train_hist.append(metric_train)
+    metric_valid_hist.append(metric_valid)
 
     if loss_valid < best_loss:
         best_loss = loss_valid
@@ -66,6 +75,7 @@ for epoch in range(config.num_epochs):
         print()
         print(f'Train: loss={loss_train:.3}')
         print(f'Valid: loss={loss_valid:.3}')
+        print()
 
 
 writer.close()
@@ -73,7 +83,13 @@ writer.close()
 # Plots: Plot learning curves
 plt.plot(range(num_epochs), loss_train_hist, 'r-', label='Train')
 plt.plot(range(num_epochs), loss_valid_hist, 'b-', label='Validation')
+plt.xlabel('Epoch')
+plt.ylabel('loss')
+plt.grid(True)
+plt.legend()
 
+plt.plot(range(num_epochs), metric_train_hist, 'r-', label='Train')
+plt.plot(range(num_epochs), metric_valid_hist, 'b-', label='Validation')
 plt.xlabel('Epoch')
 plt.ylabel('loss')
 plt.grid(True)
